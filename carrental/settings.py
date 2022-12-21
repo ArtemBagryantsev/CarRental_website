155c0e9beab99785c42639cccd5601c06818b966
ArtemBagryantsev <artembagrjancev@gmail.com>
2022-12-21 22:09:04 +1100

    setup PostgreSQL database

--- carrental/settings.py
+++ carrental/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'carrental.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carrental_db',
+        'USER': 'postgres',
+        'PASSWORD': 'pg2022',
+        'HOST': 'localhost'
     }
 }
 
