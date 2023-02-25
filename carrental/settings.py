a82413a05d8ec5f6d435f207ce7da98b92ae27b9
ArtemBagryantsev <artembagrjancev@gmail.com>
2023-02-25 21:27:39 +1100

    User registration

--- carrental/settings.py
+++ carrental/settings.py
@@ -136,6 +136,12 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
+#Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
