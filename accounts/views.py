e85a419dc771ab1e2500cb5a7c41521e605fffa1
ArtemBagryantsev <artembagrjancev@gmail.com>
2023-02-25 22:47:58 +1100

    Login-logout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,19 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid login credentials.')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
 def register(request):
@@ -40,4 +53,8 @@ def dashboard(request):
     return render(request, 'accounts/dashboard.html')
 
 def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        # messages.success(request, 'You are successfully logged out.')
+        return redirect('home')
     return redirect('home')
